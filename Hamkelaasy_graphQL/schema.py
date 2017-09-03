7807aa1c7882b216ebcdb8ce5ccfca850b2bbc11
mhazizian <mohammadh.azizian@gmail.com>
2017-09-03 12:18:29 +0430

    Support core.schema_mutation.py

--- Hamkelaasy_graphQL/schema.py
+++ Hamkelaasy_graphQL/schema.py
@@ -1,10 +1,17 @@
 import graphene
-import core.schema
+
 
 from graphene_django.debug import DjangoDebug
+import core.schema_query
+import core.schema_mutation
 
 
-class Query(core.schema.Query, graphene.ObjectType):
+class Query(core.schema_query.Query, graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name='__debug')
 
-schema = graphene.Schema(query=Query)
+
+class Mutation(core.schema_mutation.Mutation, graphene.ObjectType):
+    debug = graphene.Field(DjangoDebug, name='__debug')
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
