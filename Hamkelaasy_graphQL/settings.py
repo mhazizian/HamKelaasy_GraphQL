ba371e766ef2f924df09c42d41021263e621cf81
mhazizian <mohammadh.azizian@gmail.com>
2017-09-03 09:16:23 +0430

    Desable CORS

--- Hamkelaasy_graphQL/settings.py
+++ Hamkelaasy_graphQL/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'graphene_django',
     'rest_framework.authtoken',
     'core',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,5 @@ LOGIN_URL = '/login'
 MEDIA_URL = '/media/'
 GRAPHIQL_GRAPHQL_URL = 'graphql'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CORS_ORIGIN_ALLOW_ALL = DEBUG
