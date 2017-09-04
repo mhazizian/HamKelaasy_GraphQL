295f9a3ad70e1b8601725c7b79d5129ea1918408
mhazizian <mohammadh.azizian@gmail.com>
2017-09-04 19:20:43 +0430

    Amend tem user model

--- core/models/person/temp_user.py
+++ core/models/person/temp_user.py
@@ -6,3 +6,8 @@ from django.db import models
 class User_temp(models.Model):
     fard_access_token = models.CharField('temp for saving access token', max_length=100)
     username = models.CharField('temp for user', max_length=100)
+
+    first_name = models.CharField('first name', max_length=200, null=True, blank=True)
+    last_name = models.CharField('last name', max_length=200, null=True, blank=True)
+    email = models.CharField('email address', max_length=200, null=True, blank=True)
+    gender = models.IntegerField('gender type(1 for men, 0 for women)', default=None, null=True, blank=True)
