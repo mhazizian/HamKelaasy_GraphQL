f8c2284d66dd256d2567ac03fc60baced625fd75
mhazizian <mohammadh.azizian@gmail.com>
2017-09-04 15:41:36 +0430

    Add mutation for create kelaas

--- core/schema_mutation.py
+++ core/schema_mutation.py
@@ -2,24 +2,46 @@ from graphql import GraphQLError
 
 import graphene
 from core.models import *
-from graphql_utilz import PersonType, TagType, StudentType, MessageType
+
+from graphql_utilz import MessageType
 
 
-class PersonInput(graphene.InputObjectType):
-    username = graphene.String(required=True)
-    firstName = graphene.String(required=True)
+class Kelaas_input(graphene.InputObjectType):
+    title = graphene.String()
+    description = graphene.String()
+    tags = graphene.String()
 
 
-class CreateStudent(graphene.Mutation):
+class Create_kelaas(graphene.Mutation):
     class Arguments:
-        data = PersonInput(required=True)
+        data = Kelaas_input(required=True)
 
     Output = MessageType
 
     def mutate(self, info, data):
-        print "Add to db!!"
-        return MessageType(type="success", message="User Added to db.")
+        if info.context.user.is_authenticated:
+            user = info.context.user.person
+            if user.type == TEACHER_KEY_WORD:
+                Create_kelaas.make_kelaas(user.teacher, data)
+                return MessageType(type="success", message="Kelaas added.")
+
+        return MessageType(type="error", message="Permission denied.")
+
+    @staticmethod
+    def make_kelaas(teacher, data):
+        kelaas = Kelaas(
+            title=data.title,
+            description=data.description,
+        )
+        kelaas.save()
+        teacher.kelasses.add(kelaas)
+        teacher.save()
+        for tag_id in data.tags.split(','):
+            if Tag.objects.filter(pk=tag_id).exists():
+                tag = Tag.objects.get(pk=tag_id)
+                kelaas.tags.add(tag)
+        kelaas.save()
 
 
 class Mutation(graphene.ObjectType):
-    create_student = CreateStudent.Field()
+    create_kelaas = Create_kelaas.Field()
