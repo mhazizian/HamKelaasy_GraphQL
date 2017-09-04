f769dc1652d1509f16d52aebfbdebdb4b8e039df
mhazizian <mohammadh.azizian@gmail.com>
2017-09-04 19:04:50 +0430

    Import new mutation for adding post in schema_mutation.py

--- core/graphql_mutation/__init__.py
+++ core/graphql_mutation/__init__.py
@@ -1,2 +1,4 @@
 from .mutation_kelaas import Create_kelaas
 from .file_input import FileInput, make_file
+
+from .create_kelaas_post import Create_kelaas_post
--- core/schema_mutation.py
+++ core/schema_mutation.py
@@ -3,11 +3,9 @@ from graphql import GraphQLError
 import graphene
 from core.models import *
 
-from graphql_mutation import Create_kelaas
-
-
-
+from graphql_mutation import Create_kelaas , Create_kelaas_post
 
 
 class Mutation(graphene.ObjectType):
     create_kelaas = Create_kelaas.Field()
+    create_kelaas_post = Create_kelaas_post.Field()
