ea1371c6c4658f938173d715482537b8b3a77e93
mhazizian <mohammadh.azizian@gmail.com>
2017-09-05 10:33:49 +0430

    Fix bug

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,3 +1,3 @@
 from .index import *
 from .fard_api import Fard_API
-from .login_proccess import login, resolve_fard, signup
+from .login_proccess import login, resolve_fard, signup, temp_user_handler
