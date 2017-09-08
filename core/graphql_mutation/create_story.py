8ffeb3c183bbfd3ebd45f8242f3336f4f7e70887
mhazizian <mohammadh.azizian@gmail.com>
2017-09-08 23:28:26 +0430

    Add create_story and upload_file mutation

---
+++ core/graphql_mutation/create_story.py
@@ -0,0 +1,41 @@
+import graphene
+from core.graphql_query import MessageType
+from core.models import Story, TEACHER_KEY_WORD
+
+
+class Story_input(graphene.InputObjectType):
+    kelaas_id = graphene.Int()
+    title = graphene.String()
+    description = graphene.String()
+
+
+class Create_story(graphene.Mutation):
+    class Arguments:
+        data = Story_input(required=True)
+
+    Output = MessageType
+
+    def mutate(self, info, data):
+        if info.context.user.is_authenticated:
+            user = info.context.user.person
+            if user.type == TEACHER_KEY_WORD:
+                if user.teacher.kelaases.filter(pk=data.kelaas_id).exists():
+                    kelaas = user.teacher.kelaases.get(pk=data.kelaas_id)
+                    Create_story.make_story(info, kelaas, data, user.teacher)
+                    return MessageType(type="success", message="Story added.")
+
+        return MessageType(type="error", message="Permission denied.")
+
+    @staticmethod
+    def make_story(info, kelaas, data, teacher):
+        story = Story(
+            title=data.title,
+            description=data.description,
+            kelaas=kelaas,
+            owner=teacher,
+        )
+        story.save()
+
+        if 'pic' in info.context.FILES:
+            story.story_pic = info.context.FILES['pic']
+            story.save()
--- core/graphql_mutation/file_input.py
+++ core/graphql_mutation/file_input.py
@@ -1,17 +1,41 @@
 import graphene
-from core.models import File
+
+from core.graphql_query import MessageType
+from core.models import TEACHER_KEY_WORD, File
 
 
-class FileInput(graphene.InputObjectType):
-    url = graphene.String(required=True)
+class File_input(graphene.InputObjectType):
     title = graphene.String()
+    description = graphene.String(default_value="")
 
 
-def make_file(data, owner):
-    file = File(
-        title=data.title,
-        url=data.url,
-        owner=owner,
-    )
-    file.save()
-    return file
+class Upload_file(graphene.Mutation):
+    class Arguments:
+        data = File_input(required=True)
+
+    Output = MessageType
+
+    def mutate(self, info, data):
+        if info.context.user.is_authenticated:
+            user = info.context.user.person
+            if user.type == TEACHER_KEY_WORD:
+                res = Upload_file.upload(info, data)
+                return MessageType(type="success", message=res)
+
+        return MessageType(type="error", message="Permission denied.")
+
+    @staticmethod
+    def upload(info, data):
+        uploaded_file = []
+        for f in info.context.FILES.getlist('post-files'):
+            temp = File(
+                title=data.title,
+                description=data.description,
+                data=f,
+            )
+            temp.owner = info.context.user.person
+            temp.save()
+            uploaded_file.append(temp)
+
+        # return [f.id for f in uploaded_file]
+        return ''.join([str(f.id) for f in uploaded_file])
