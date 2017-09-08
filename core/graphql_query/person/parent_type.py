ea4214f8967ea7dca445838f7d463d2d2b3f63c3
mhazizian <mohammadh.azizian@gmail.com>
2017-09-08 11:21:30 +0430

    Add childes quey on parentType

--- core/graphql_query/person/parent_type.py
+++ core/graphql_query/person/parent_type.py
@@ -9,13 +9,12 @@ from core.models import PARENT_KEY_WORD
 class ParentType(PersonType):
     name = "parent"
 
-    person = graphene.Field('core.graphql_query.PersonType')
-    students = graphene.List('core.graphql_query.StudentType')
+    childes = graphene.List('core.graphql_query.StudentType')
 
-    def resolve_students(self, info):
+    def resolve_childs(self, info):
         user = info.context.user.person
 
         if it_is_him(self, user):
-            return self.students.all()
+            return self.childes.all()
 
         raise GraphQLError('Permission denied')
