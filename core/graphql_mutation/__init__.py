8e8380c39198806c456ffdb86a4d11a00fde4982
mhazizian <mohammadh.azizian@gmail.com>
2017-09-08 23:32:31 +0430

    Fix bug on wrong importing

--- core/graphql_mutation/__init__.py
+++ core/graphql_mutation/__init__.py
@@ -1,4 +1,4 @@
 from .mutation_kelaas import Create_kelaas
 from .create_kelaas_post import Create_kelaas_post
 from .create_story import Create_story
-from .file_input import File_input
+from .file_input import Upload_file
--- core/schema_mutation.py
+++ core/schema_mutation.py
@@ -3,11 +3,11 @@ from graphql import GraphQLError
 import graphene
 from core.models import *
 
-from graphql_mutation import Create_kelaas , Create_kelaas_post, Create_story, File_input
+from graphql_mutation import Create_kelaas , Create_kelaas_post, Create_story, Upload_file
 
 
 class Mutation(graphene.ObjectType):
     create_kelaas = Create_kelaas.Field()
     create_kelaas_post = Create_kelaas_post.Field()
     create_story = Create_story.Field()
-    upload_file = File_input.Field()
+    upload_file = Upload_file.Field()
