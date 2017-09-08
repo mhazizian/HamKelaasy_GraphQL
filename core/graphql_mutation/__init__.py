e289cdaeb00fb7630deec7cc2f77758d50d0124b
mhazizian <mohammadh.azizian@gmail.com>
2017-09-08 23:24:59 +0430

    Add Story mutation

--- core/graphql_mutation/__init__.py
+++ core/graphql_mutation/__init__.py
@@ -1,4 +1,4 @@
 from .mutation_kelaas import Create_kelaas
-from .file_input import FileInput, make_file
-
 from .create_kelaas_post import Create_kelaas_post
+from .create_story import Create_story
+from .file_input import File_input
