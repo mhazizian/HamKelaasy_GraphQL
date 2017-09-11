13ff40e88e05c5f055aa0c2ad876eeafa3bfb510
mhazizian <mohammadh.azizian@gmail.com>
2017-09-11 09:31:59 +0430

    Fix bug on premission checking func

--- core/graphql_query/utilz.py
+++ core/graphql_query/utilz.py
@@ -3,7 +3,7 @@ from graphql import GraphQLError
 
 def it_is_him(obj1, obj2):
     if not obj1.id == obj2.id:
-        raise GraphQLError('Permission denied')
+        return False
     return True
 
 
