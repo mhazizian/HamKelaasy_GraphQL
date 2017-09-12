f4bec387e176d33f22136ae2fd47a4d6107cd915
mhazizian <mohammadh.azizian@gmail.com>
2017-09-12 08:19:55 +0430

    Add some log for debugging to index view

--- core/views/index.py
+++ core/views/index.py
@@ -10,15 +10,21 @@ from Hamkelaasy_graphQL.schema import schema
 
 @api_view(['POST'])
 def index(request):
+    print "_______________________________________________________"
     if not request.user.is_authenticated:
         return HttpResponse('user not authenticated', status=401)
 
     if request.method == "POST":
         data = json.loads(request.body)
+        print ">>> request:"
+        print data.get('query', '')
 
         res = schema.execute(data.get('query', ''), context_value=request)
         if res.errors:
+            print res.errors[0].message
             return HttpResponse(res.errors[0].message, status=400)
+        print ">>> respond:"
+        print json.dumps(res.data, indent=4, sort_keys=True)
         return HttpResponse(json.dumps(res.data), content_type='application/json', status=200)
 
     return HttpResponse("not post method!", status=405)
