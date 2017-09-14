105615cd5cf21def7e3647f194cb09f4606a7b33
mhazizian <mohammadh.azizian@gmail.com>
2017-09-14 20:10:42 +0430

    Amen to work with new exeptions class

--- core/graphql_utilz.py
+++ core/graphql_utilz.py
@@ -1,6 +1,9 @@
+from graphql import GraphQLError
 
-class myGraphQLError(Exception):
+
+class myGraphQLError(GraphQLError):
     def __init__(self, message, status=400, detail=''):
+        super(myGraphQLError, self).__init__(message)
         self.message = message
         self.status = status
         self.detail = detail
--- core/schema_query.py
+++ core/schema_query.py
@@ -1,107 +1,129 @@
-from core import myGraphQLError
-
+import exceptions
 import graphene
+
+from core import myGraphQLError
 from core.models import *
 from graphql_query import *
 
 
 def resolve_student(root, info, **kwargs):
-    if info.context.user.is_authenticated:
-        user = info.context.user.person
-        if user.type == STUDENT_KEY_WORD:
-            return user.student
+    if not info.context.user.is_authenticated:
+        raise myGraphQLError('user not authenticated', status=401)
+    user = info.context.user.person
 
-        id = kwargs['id']
-        if user.type == PARENT_KEY_WORD:
-            if user.parent.childes.filter(pk=id).exists():
-                return user.parent.childes.get(pk=id)
-            raise myGraphQLError('Student not found')
+    if user.type == STUDENT_KEY_WORD:
+        return user.student
 
-        if user.type == TEACHER_KEY_WORD:
-            for kelaas in user.teacher.kelaases.all():
-                if kelaas.students.filter(id=id).exists():
-                    return Student.objects.get(pk=id)
+    if not 'id' in kwargs:
+        raise myGraphQLError('"id" is necessary', status=400)
+    id = kwargs['id']
 
-    raise myGraphQLError('Permission denied')
+    if user.type == PARENT_KEY_WORD:
+        try:
+            return user.parent.childes.get(pk=id)
+        except Student.DoesNotExist:
+            raise myGraphQLError('Student not found', status=404)
+
+    if user.type == TEACHER_KEY_WORD:
+        for teacher_kelaas in user.teacher.kelaases.all():
+            if teacher_kelaas.students.filter(pk=id).exists():
+                return teacher_kelaas.students.get(pk=id)
+        raise myGraphQLError('Student not found', status=404)
 
 
 def resolve_students(root, info, **kwargs):
-    if info.context.user.is_authenticated:
+    if not info.context.user.is_authenticated:
+        raise myGraphQLError('user not authenticated', status=401)
+    user = info.context.user.person
 
-        page_size = kwargs.get('page_size', DEFAULT_PAGE_SIZE)
-        offset = kwargs.get('page', 1) * page_size
+    page_size = kwargs.get('page_size', DEFAULT_PAGE_SIZE)
+    offset = kwargs.get('page', 1) * page_size
 
-        user = info.context.user.person
+    if user.type == PARENT_KEY_WORD:
+        return user.parent.childes.all()[offset - page_size:offset]
 
-        if user.type == PARENT_KEY_WORD:
-            return user.parent.childes.all()[offset - page_size:offset]
+    if user.type == TEACHER_KEY_WORD:
+        if 'kelaas_id' in kwargs:
+            try:
+                return Kelaas.objects.get(pk=kwargs['kelaas_id']).students.all()[offset - page_size:offset]
+            except Kelaas.DoesNotExist:
+                raise myGraphQLError('Kelaas not found', status=404)
 
-        if user.type == TEACHER_KEY_WORD:
-            if 'kelaas_id' in kwargs:
-                if user.teacher.kelaases.filter(id=kwargs['kelaas_id']).exists():
-                    return Kelaas.objects.get(pk=kwargs['kelaas_id']).students.all()[offset - page_size:offset]
-
-    raise myGraphQLError('Permission denied')
+    raise myGraphQLError('Permission denied', status=403)
 
 
 def resolve_kelaas(root, info, id):
-    if info.context.user.is_authenticated:
-        user = info.context.user.person
+    if not info.context.user.is_authenticated:
+        raise myGraphQLError('user not authenticated', status=401)
+    user = info.context.user.person
 
-        if user.type == TEACHER_KEY_WORD:
-            if user.teacher.kelaases.filter(id=id).exists():
-                return user.teacher.kelaases.get(pk=id)
-        if user.type == PARENT_KEY_WORD:
-            for student in user.parent.childes.all():
-                if student.kelaases.filter(pk=id).exists():
-                    return student.kelaases.get(pk=id)
-        if user.type == STUDENT_KEY_WORD:
-            if user.student.kelaases.filter(pk=id).exists():
-                return user.student.kelaases.get(pk=id)
-    raise myGraphQLError('Permission denied')
+    if user.type == TEACHER_KEY_WORD:
+        try:
+            return user.teacher.kelaases.get(pk=id)
+        except Kelaas.DoesNotExist:
+            raise myGraphQLError('Kelaas not found', status=404)
+
+    if user.type == PARENT_KEY_WORD:
+        for student in user.parent.childes.all():
+            if student.kelaases.filter(pk=id).exists():
+                return student.kelaases.get(pk=id)
+        raise myGraphQLError('Kelaas not found', status=404)
+
+    if user.type == STUDENT_KEY_WORD:
+        try:
+            return user.student.kelaases.get(pk=id)
+        except Kelaas.DoesNotExist:
+            raise myGraphQLError('Kelaas not found', status=404)
 
 
 def resolve_kelaases(root, info, **kwargs):
-    if info.context.user.is_authenticated:
-        user = info.context.user.person
+    if not info.context.user.is_authenticated:
+        raise myGraphQLError('user not authenticated', status=401)
+    user = info.context.user.person
 
-        page_size = kwargs.get('page_size', DEFAULT_PAGE_SIZE)
-        offset = kwargs.get('page', 1) * page_size
+    page_size = kwargs.get('page_size', DEFAULT_PAGE_SIZE)
+    offset = kwargs.get('page', 1) * page_size
 
-        if user.type == TEACHER_KEY_WORD:
-            return user.teacher.kelaases.all()[offset - page_size:offset]
-        if user.type == PARENT_KEY_WORD:
-            if 'student_id' in kwargs:
-                if user.parent.childes.filter(pl=kwargs['student_id']).exists():
-                    return user.parent.childes.get(pk=kwargs['student_id']).kelaases.all()[offset - page_size:offset]
+    if user.type == TEACHER_KEY_WORD:
+        return user.teacher.kelaases.all()[offset - page_size:offset]
 
-        if user.type == STUDENT_KEY_WORD:
-            return user.student.kelaases.all()[offset - page_size:offset]
-    raise myGraphQLError('Permission denied')
+    if user.type == PARENT_KEY_WORD:
+        try:
+            return user.parent.childes.get(pk=kwargs['student_id']).kelaases.all()[offset - page_size:offset]
+        except exceptions.KeyError:
+            raise myGraphQLError('"student_id" is necessary', status=400)
+        except Student.DoesNotExist:
+            raise myGraphQLError('Student not found', status=404)
+
+    if user.type == STUDENT_KEY_WORD:
+        return user.student.kelaases.all()[offset - page_size:offset]
 
 
 def resolve_teacher(root, info):
-    if info.context.user.is_authenticated:
-        user = info.context.user.person
+    if not info.context.user.is_authenticated:
+        raise myGraphQLError('user not authenticated', status=401)
+    user = info.context.user.person
 
-        if user.type == TEACHER_KEY_WORD:
-            return user.teacher
-    raise myGraphQLError('Permission denied')
+    if user.type == TEACHER_KEY_WORD:
+        return user.teacher
+
+    raise myGraphQLError('Permission denied', status=403)
 
 
 def resolve_parent(root, info):
-    if info.context.user.is_authenticated:
-        user = info.context.user.person
+    if not info.context.user.is_authenticated:
+        raise myGraphQLError('user not authenticated', status=401)
+    user = info.context.user.person
 
-        if user.type == PARENT_KEY_WORD:
-            return user.parent
-    raise myGraphQLError('Permission denied')
+    if user.type == PARENT_KEY_WORD:
+        return user.parent
+    raise myGraphQLError('Permission denied', status=403)
 
 
 def resolve_me(root, info):
-    if info.context.user.is_authenticated:
-        return info.context.user.person
-    raise myGraphQLError('Permission denied')
+    if not info.context.user.is_authenticated:
+        raise myGraphQLError('user not authenticated', status=401)
+    return info.context.user.person
 
 
 def resolve_badge_types(root, info, **kwargs):
@@ -116,8 +138,11 @@ def resolve_badge_types(root, info, **kwargs):
 
 
 def resolve_certificate(root, info, id):
-    if Certificate.objects.filter(pk=id).exists():
+    try:
         return Certificate.objects.get(pk=id)
+    except Certificate.DoesNotExist:
+        raise myGraphQLError('Certificate not found', status=404)
+
 
 def resolve_tags(root, info):
     return Tag.objects.all()
