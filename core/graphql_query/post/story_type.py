01e55dc2ad21c343786de98b49233433c379c9da
mhazizian <mohammadh.azizian@gmail.com>
2017-09-16 10:38:34 +0430

    Add like_count filed to story object

--- core/graphql_query/post/story_type.py
+++ core/graphql_query/post/story_type.py
@@ -6,8 +6,12 @@ class StoryType(PostType):
     name = "story"
 
     pic = graphene.String()
+    like_count = graphene.Int()
 
     def resolve_pic(self, info):
         return self.story.pic
+
+    def resolve_like_count(self, info):
+        return self.story.like_count
 
 
