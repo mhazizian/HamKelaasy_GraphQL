8e5a30d3899dcebdc049ef05fe66af2e769e63e3
mhazizian <mohammadh.azizian@gmail.com>
2017-09-17 09:52:57 +0430

    Add comment and code refactor

--- Hamkelaasy_graphQL/schema.py
+++ Hamkelaasy_graphQL/schema.py
@@ -1,17 +1,23 @@
 import graphene
 
-
-from graphene_django.debug import DjangoDebug
-import core.schema_query
-import core.schema_mutation
+from core.schema_query import Query as core_query
+from core.schema_mutation import Mutation as core_mutation
 
 
-class Query(core.schema_query.Query, graphene.ObjectType):
-    debug = graphene.Field(DjangoDebug, name='__debug')
+class Query(core_query, graphene.ObjectType):
+    class Meta:
+        description = "List query support pagination,\n\n" \
+                      "in order to user pageination usr two var 'page', 'pageSize'\n\n" \
+                      "by default page=1 and if you dont provide any value for them you'll only get first page" \
+                      "(for example first 10 item in case pageSize=10)\n\n" \
+                      "for example:\n\n" \
+                      "(page=2 , pageSize=20)\n\nwill give items from 20 to 40(num 40 not included)"
 
 
-class Mutation(core.schema_mutation.Mutation, graphene.ObjectType):
-    debug = graphene.Field(DjangoDebug, name='__debug')
+class Mutation(core_mutation, graphene.ObjectType):
+    class Meta:
+        description = "the result of a Mutation is based on its kind, " \
+                      "and in case if failer result will only contain 'error'"
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
