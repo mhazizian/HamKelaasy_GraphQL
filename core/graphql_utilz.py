0caa5137c8964eb7f93277d06ca88fd78e76f23f
mhazizian <mohammadh.azizian@gmail.com>
2017-09-18 12:24:27 +0430

    Reafactor

--- core/graphql_utilz.py
+++ core/graphql_utilz.py
@@ -1,4 +1,6 @@
+import six
 from graphql import GraphQLError
+from graphql.error import format_error as format_graphql_error
 
 
 class myGraphQLError(GraphQLError):
@@ -7,3 +9,30 @@ class myGraphQLError(GraphQLError):
         self.message = message
         self.status = status
         self.detail = detail
+
+
+def get_status_code(response):
+    if response.errors:
+        status_code = 400
+        if hasattr(response.errors[0], 'original_error'):
+            if isinstance(response.errors[0].original_error, myGraphQLError):
+                status_code = response.errors[0].original_error.status
+        return status_code
+    else:
+        return 200
+
+
+def get_pretty_response(response):
+    res = {}
+    if response.errors:
+        res['errors'] = [format_error(e) for e in response.errors]
+
+    res['data'] = response.data
+    return res
+
+
+def format_error(error):
+    if isinstance(error, GraphQLError):
+        return format_graphql_error(error)
+
+    return {'message': six.text_type(error)}
--- core/views/index.py
+++ core/views/index.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import json
-import six
 
-from core import myGraphQLError
-from graphql import GraphQLError
-from graphql.error import format_error as format_graphql_error
-
+from core.graphql_utilz import get_status_code, get_pretty_response
 from Hamkelaasy_graphQL.schema import schema
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
@@ -14,60 +10,37 @@ from django.http import HttpResponse
 
 @api_view(['POST'])
 def index(request):
-    print "_______________________________________________________"
     if not request.user.is_authenticated or not hasattr(request.user, 'person'):
         return HttpResponse('user not authenticated', status=401)
+    data = json.loads(request.body)
 
-    if request.method == "POST":
-        data = json.loads(request.body)
-        print ">>> request:"
-        print data.get('query', '')
-        print data.get('variables', None)
-        print ">>> respond:"
+    print "_______________________________________________________"
+    print ">>> request:"
+    print data.get('query', '')
+    print data.get('variables', None)
+    print ">>> response:"
 
-        res = schema.execute(
-            data.get('query', ''),
-            context_value=request,
-            operation_name=data.get('operationName', None),
-            variable_values=data.get('variables', None),
-        )
-        response = {}
-        if res.errors:
+    res = schema.execute(
+        data.get('query', ''),
+        context_value=request,
+        operation_name=data.get('operationName', None),
+        variable_values=data.get('variables', None),
+    )
 
-            response['errors'] = [format_error(e) for e in res.errors]
-            response['data'] = res.data
+    response = get_pretty_response(res)
+    status_code = get_status_code(res)
 
-            print json.dumps(response, indent=4, sort_keys=True)
+    print json.dumps(response, indent=4, sort_keys=True)
 
-            status_code = 400
-            if isinstance(res.errors[0], myGraphQLError):
-                status_code = res.errors[0].original_error.status
-
-            # TODO change 'res.errors[0].message' to 'json.dumps(response)'
-            return HttpResponse(
-                res.errors[0].message,
-                status=status_code,
-                content_type='application/json',
-            )
-
-        response['data'] = res.data
-        print json.dumps(response, indent=4, sort_keys=True)
-
-        # TODO change the format of response
-        return HttpResponse(json.dumps(res.data), content_type='application/json', status=200)
-
-    return HttpResponse("not post method!", status=405)
+    # TODO change 'response['data']' to 'response'
+    return HttpResponse(
+        json.dumps(response['data']),
+        status=status_code,
+        content_type='application/json',
+    )
 
 
 @api_view(['POST', 'GET'])
 def logout(request):
     # TODO expire given token
-
     return HttpResponse('')
-
-
-def format_error(error):
-    if isinstance(error, GraphQLError):
-        return format_graphql_error(error)
-
-    return {'message': six.text_type(error)}
