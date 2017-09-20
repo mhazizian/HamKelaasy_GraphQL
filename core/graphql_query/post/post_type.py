f0bec12d8433711a2c731a5c8c0ebdde265e6b55
mhazizian <mohammadh.azizian@gmail.com>
2017-09-20 20:44:17 +0430

    Add comment_count query on posts

--- core/graphql_query/post/post_type.py
+++ core/graphql_query/post/post_type.py
@@ -19,11 +19,15 @@ class PostType(graphene.ObjectType):
         page_size=graphene.Int(),
         page=graphene.Int(),
     )
+    comment_count = graphene.Int()
 
     def resolve_comments(self, info, **kwargs):
         page_size = kwargs.get('page_size', DEFAULT_PAGE_SIZE)
         offset = kwargs.get('page', 1) * page_size
         return self.comments.all().order_by('-id')[offset - page_size:offset]
+
+    def resolve_comment_count(self, info):
+        return self.comments.count()
 
     def resolve_owner(self, info):
         return self.owner
