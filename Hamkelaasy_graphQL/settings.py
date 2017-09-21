e41e8411c184e9ecc433f0eaa3ecf146e9ad7c77
mhazizian <mohammadh.azizian@gmail.com>
2017-09-21 15:05:48 +0430

    Set DEBUG to False and reafsctor

--- Hamkelaasy_graphQL/settings.py
+++ Hamkelaasy_graphQL/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -24,14 +23,13 @@ SECRET_KEY = 'qqce@wq!=3x=%-hv8((gh&^s81lv=bf^#as327j194igg)03qf'
 SERVER_ADDR = "http://94.182.227.193:9090/"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "0.0.0.0",
     "127.0.0.1",
     "94.182.227.193",
 ]
-
 
 # Application definition
 
@@ -47,8 +45,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core',
 ]
-
-
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
@@ -66,7 +62,7 @@ ROOT_URLCONF = 'Hamkelaasy_graphQL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,  'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Hamkelaasy_graphQL.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -90,8 +85,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'mydatabase',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'borhan123',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -111,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -125,14 +126,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 GRAPHENE = {
     'SCHEMA': 'Hamkelaasy_graphQL.schema.schema',
     'MIDDLEWARE': [
-            'graphene_django.debug.DjangoDebugMiddleware',
-        ]
+        'graphene_django.debug.DjangoDebugMiddleware',
+    ]
 }
 GRAPHIQL_GRAPHQL_URL = 'graphql'
 
@@ -142,12 +142,10 @@ REST_FRAMEWORK = {
     ),
 }
 
-
 LOGIN_URL = '/login'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = (
