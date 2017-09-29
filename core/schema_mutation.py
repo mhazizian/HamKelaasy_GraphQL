569da64d7579a8bd0c48e06480b762e56647d498
mhazizian <mohammadh.azizian@gmail.com>
2017-09-29 10:42:58 +0330

    Add delete comment mutation to main mutations

--- core/schema_mutation.py
+++ core/schema_mutation.py
@@ -2,7 +2,7 @@ import graphene
 
 from graphql_mutation import Create_kelaas, Create_kelaas_post, Create_story, Upload_file, Assign_badge, Add_comment, \
     Join_kelaas, Add_child, Send_message, Assign_certificate, Create_certificate, \
-    Create_certificate_level, Perform_task, Add_child_by_token
+    Create_certificate_level, Perform_task, Add_child_by_token, Delete_comment
 
 
 class Mutation(graphene.ObjectType):
@@ -26,3 +26,4 @@ class Mutation(graphene.ObjectType):
     create_certificate_level = Create_certificate_level.Field()
 
     perform_task = Perform_task.Field()
+    delete_comment = Delete_comment.Field()
