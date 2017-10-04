b621e381ad6610acfd316408f04c98658840617b
mhazizian <mohammadh.azizian@gmail.com>
2017-10-04 10:56:39 +0330

    Amend router to catch endpoint ending in /

--- Hamkelaasy_graphQL/settings.py
+++ Hamkelaasy_graphQL/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'Hamkelaasy_graphQL.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hamkelaasy',
-        'USER': 'admin',
-        'PASSWORD': 'oskolback',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'hamkelaasy',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'oskolback',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 # Password validation
--- core/urls.py
+++ core/urls.py
@@ -9,29 +9,37 @@ urlpatterns = [
 
     # GraphQL main address:
     url(r'^graphql$', views.index, name='index'),
+    url(r'^graphql/$', views.index),
 
     # logout url
     url(r'^logout$', views.logout, name='logout'),
+    url(r'^logout/$', views.logout),
 
     # Upload file endpoint:
     url(r'^file/upload$', views.upload_file, name='upload'),
+    url(r'^file/upload/$', views.upload_file),
 
     # endpoint for redirecting to fard.ir with provided redirect_url
     url(r'^fard/login$', views.login, name='login'),
+    url(r'^fard/login/$', views.login),
 
     # endpoint for sending user_data for registration and getting login token
     url(r'^fard/register$', views.signup, name='signup'),
+    url(r'^fard/register/$', views.signup),
 
     # endpoint for receiving data from user_temp table
     url(r'^fard/temp/data$', views.temp_user_handler, name='signup'),
+    url(r'^fard/temp/data/$', views.temp_user_handler),
 
     # Listening url for getting data from fard.ir:
     url(r'^' + views.Fard_API().listening_local_url + '$', views.resolve_fard, name='fard-listening'),
 
     # for getting besic kelaas information using kelaas invite code
     url(r'^kelaas/basic_info$', views.get_kelaas_basic_info_handler, name='kelaas-basic-information'),
+    url(r'^kelaas/basic_info/$', views.get_kelaas_basic_info_handler),
 
     url(r'^apply', views.my_view, name='test'),
+    url(r'^apply/', views.my_view),
 
 
 ]
