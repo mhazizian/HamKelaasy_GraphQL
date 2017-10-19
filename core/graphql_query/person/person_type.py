5ed4946a35ee5c8d947501f2b87f44157d85339a
mhazizian <mohammadh.azizian@gmail.com>
2017-10-19 19:50:46 +0330

    Add checker query for new password and setted phone number'

--- core/graphql_query/person/person_type.py
+++ core/graphql_query/person/person_type.py
@@ -13,9 +13,22 @@ class PersonType(graphene.ObjectType):
     signup_completed = graphene.Boolean()
     type = graphene.String()
 
+    # TODO : remove these two after some weak
+    has_new_password = graphene.Boolean()
+    has_phone_number = graphene.Boolean()
     # for test only:
     username = graphene.String()
 
     def resolve_username(self, info):
         if self.user:
             return self.user.username
+
+    def resolve_has_phone_number(self, info):
+        if self.phone_number == '':
+            return False
+        return True
+
+    def resolve_has_mew_password(self, info):
+        if self.has_new_password:
+            return True
+        return False
