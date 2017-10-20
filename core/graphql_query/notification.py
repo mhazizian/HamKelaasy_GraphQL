c8efe96ef4f3a3d2cfc9b49b970b4a888bc0bfe1
mhazizian <mohammadh.azizian@gmail.com>
2017-10-20 11:11:10 +0330

    Add id field to notification type

--- core/graphql_query/notification.py
+++ core/graphql_query/notification.py
@@ -4,6 +4,7 @@ import graphene
 class NotificationType(graphene.ObjectType):
     name = "notification"
 
+    id = graphene.Int()
     receiver = graphene.Field('core.graphql_query.PersonType')
     has_seen = graphene.Int()
 
