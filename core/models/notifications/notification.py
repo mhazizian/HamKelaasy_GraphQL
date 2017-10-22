98fd7a6232205e0e46c01e90ee49ebc9a3d47733
mhazizian <mohammadh.azizian@gmail.com>
2017-10-22 17:36:25 +0330

    Add create func for Teacher's ntification + amend notification doc and API

--- core/models/notifications/notification.py
+++ core/models/notifications/notification.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from core.utilz import pretty_past_time, to_shamsi_date
+from .notification_doc import Notification_type
 
 
 class Notification(models.Model):
@@ -27,3 +28,47 @@ class Notification(models.Model):
     def time_passed(self):
         delta = timezone.now() - self.create_date
         return pretty_past_time(delta)
+
+    @staticmethod
+    def create_teacher__new_student(teacher, student, kelaas):
+        notif = Notification(
+            receiver=teacher,
+            type_code=Notification_type.Teacher.new_student,
+            related_ids=str(kelaas.id) + ',' + str(student.id),
+            related_text=student.first_name + ',' + student.last_name
+        )
+        notif.save()
+        return notif
+
+    @staticmethod
+    def create_teacher__new_parent(teacher, student, kelaas):
+        notif = Notification(
+            receiver=teacher,
+            type_code=Notification_type.Teacher.new_parent,
+            related_ids=str(kelaas.id) + ',' + str(student.id) + ',' + str(student.parents.id),
+            related_text=student.first_name + ',' + student.last_name
+        )
+        notif.save()
+        return notif
+
+    @staticmethod
+    def create_teacher__new_message(teacher, message, kelaas):
+        notif = Notification(
+            receiver=teacher,
+            type_code=Notification_type.Teacher.new_message,
+            related_ids=str(message.conversation.id) + ',' + str(kelaas.id) + ',' + str(message.id),
+            related_text=message.body
+        )
+        notif.save()
+        return notif
+
+    @staticmethod
+    def create_teacher__new_comment(teacher, comment, kelaas):
+        notif = Notification(
+            receiver=teacher,
+            type_code=Notification_type.Teacher.new_comment,
+            related_ids=str(comment.post.id) + ',' + str(kelaas.id) + ',' + str(comment.owner.id),
+            related_text=comment.owner.first_name + ',' + comment.owner.last_name + ',' + comment.body
+        )
+        notif.save()
+        return notif
--- core/models/notifications/notification_doc.py
+++ core/models/notifications/notification_doc.py
@@ -4,9 +4,9 @@ from enum import Enum
 class Notification_type(object):
     class Teacher(Enum):
         new_student = 10001
-        new_message = 10002  # also will have conversation id
-        new_comment = 10003  # also will have post id
-        new_parent = 10004  # also will have student id
+        new_message = 10002
+        new_comment = 10003
+        new_parent = 10004
 
     class Parent(Enum):
         new_message = 20001  # also will have conversation id
@@ -23,21 +23,21 @@ class Notification_type(object):
 notification_doc = {
     Notification_type.Teacher.new_student.value: {
         'related_ids': 'kelaas_id, sutdent_id',
-        'related_text': 'fist name and last name of new student',
+        'related_text': 'student_fistname, student_lastname',
         'description': 'notification for new student joined kelasses',
     },
     Notification_type.Teacher.new_message.value: {
-        'related_ids': 'conversation_id, kelaas_id',
-        'related_text': 'new message body',
+        'related_ids': 'conversation_id, kelaas_id, message_id',
+        'related_text': 'message_body',
         'description': 'notification for new message in teachers conversation with parents',
     },
     Notification_type.Teacher.new_comment.value: {
-        'related_ids': 'post_id, kelaas_id',
-        'related_text': 'new comment body',
+        'related_ids': 'post_id, kelaas_id, writer_id',
+        'related_text': 'writer_firstname, writer_lastname, comment_body',
         'description': 'notification for new comment in teachers related kelaases',
     },
     Notification_type.Teacher.new_parent.value: {
-        'related_ids': 'kelaas_id, student_id',
+        'related_ids': 'kelaas_id, student_id, parent_id',
         'related_text': 'student first name and last name',
         'description': 'notification for new parent joined kelaas',
     },
