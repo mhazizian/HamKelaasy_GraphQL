d4b8c2251464d9695f2dd9c8da2b2adfcff3d834
mhazizian <mohammadh.azizian@gmail.com>
2017-10-25 12:03:09 +0330

    Add active users in info page

--- core/views/info.py
+++ core/views/info.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import json
 import logging
 
+from django.contrib.auth.models import User
 from django.shortcuts import render
 import core.services as services
 
@@ -14,7 +15,10 @@ logger = logging.getLogger('core')
 
 
 def info(request):
-    time = datetime.now() - timedelta(hours=24)
+    last_hour = datetime.now() - timedelta(hours=1)
+    yesterday = datetime.now() - timedelta(hours=24)
+    last_week = datetime.now() - timedelta(days=7)
+    last_month = datetime.now() - timedelta(days=30)
 
     return render(request, 'core/info.html', {
         'user_count': Person.objects.count(),
@@ -27,14 +31,19 @@ def info(request):
         'comment_count': Comment.objects.count(),
         'message_count': Conversation_message.objects.count(),
 
-        'today_user_count': Person.objects.filter(create_date__gte=time).count(),
-        'today_parent_count': Parent.objects.filter(create_date__gte=time).count(),
-        'today_student_count': Student.objects.filter(create_date__gte=time).count(),
-        'today_teacher_count': Teacher.objects.filter(create_date__gte=time).count(),
-        'today_kelaas_count': Kelaas.objects.filter(create_date__gte=time).count(),
-        'today_story_count': Story.objects.filter(create_date__gte=time).count(),
-        'today_message_count': Conversation_message.objects.filter(create_date__gte=time).count(),
+        'today_user_count': Person.objects.filter(create_date__gte=yesterday).count(),
+        'today_parent_count': Parent.objects.filter(create_date__gte=yesterday).count(),
+        'today_student_count': Student.objects.filter(create_date__gte=yesterday).count(),
+        'today_teacher_count': Teacher.objects.filter(create_date__gte=yesterday).count(),
+        'today_kelaas_count': Kelaas.objects.filter(create_date__gte=yesterday).count(),
+        'today_story_count': Story.objects.filter(create_date__gte=yesterday).count(),
+        'today_message_count': Conversation_message.objects.filter(create_date__gte=yesterday).count(),
 
         'user_with_new_password': Person.objects.filter(has_new_password=True).count(),
         'user_with_phone': Person.objects.filter(phone_number_verified=True).count(),
+
+        'used_service_within_last_hour': User.objects.filter(last_login__gt=last_hour).count(),
+        'used_service_within_24_hr': User.objects.filter(last_login__gt=yesterday).count(),
+        'used_service_within_last_week': User.objects.filter(last_login__gt=last_month).count(),
+        'used_service_within_last_month': User.objects.filter(last_login__gt=last_month).count(),
     })
