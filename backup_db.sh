385da1eeb13d6d9229710361a3531f2819b3074d
mhazizian <mohammadh.azizian@gmail.com>
2017-11-03 20:00:48 +0330

    Amend backup database script

--- backup_db.sh
+++ backup_db.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-time=$(date +%Y_%m_%d_%A_%H_%M_%S)
-backup_file="./db_backup/"$time".json"
-./manage.py dumpdata > $backup_file
+RUNAT="02"
+
+while [ 1 ]
+do
+    DATE=`/bin/date +%H`
+    if [ $DATE. = $RUNAT. ]
+    then
+        time=$(date +%Y_%m_%d_%A_%H_%M_%S)
+		backup_file="./db_backup/"$time".json"
+		backup_file_enc=$backup_file".enc"
+
+		./manage.py dumpdata > $backup_file
+
+		openssl aes-256-cbc -a -salt -in $backup_file -out $backup_file_enc -pass pass:borhan123
+		./gdrive upload -p 0B9kxC0hDGmDLeTdCSXh2UWFBU2c $backup_file_enc
+    fi
+
+    sleep 3590
+done
+
+
+#openssl aes-256-cbc -d -a -in $backup_file_enc -out secrets.txt.new -pass pass:borhan123
