ea2a36f892a5df4c1a63b11bcfa4d5080e3d815a
mhazizian <mohammadh.azizian@gmail.com>
2017-11-22 15:33:21 +0330

    Add owner field next with same function as writer in ConversationMessageType class

--- core/graphql_query/chat/message_type.py
+++ core/graphql_query/chat/message_type.py
@@ -1,9 +1,11 @@
 import graphene
 import core.services as services
 
+
 class ConversationMessageType(graphene.ObjectType):
     body = graphene.String()
     writer = graphene.Field('core.graphql_query.PersonType')
+    owner = graphene.Field('core.graphql_query.PersonType')
 
     is_my_message = graphene.Boolean()
 
@@ -14,4 +16,5 @@ class ConversationMessageType(graphene.ObjectType):
         user = info.context.user.person
         return services.messages__is_my_message(message=self, user=user)
 
-
+    def resolve_owner(self, info):
+        return self.writer
