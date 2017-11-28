e6a1fc9c9485479e3daa53c340e84fe1dd47b03d
mhazizian <mohammadh.azizian@gmail.com>
2017-11-28 13:15:10 +0330

    Add child query to personType only if requested user is parent

--- core/graphql_query/person/parent_type.py
+++ core/graphql_query/person/parent_type.py
@@ -6,25 +6,3 @@ import core.services as services
 
 class ParentType(PersonType):
     name = "parent"
-
-    childes = graphene.List(
-        'core.graphql_query.StudentType',
-        page_size=graphene.Int(default_value=services.DEFAULT_PAGE_SIZE),
-        page=graphene.Int(default_value=1),
-        kelaas_id=graphene.Int(description="privdes childern of a spacific kelaas"
-                                           "(now, it only works if query is requested by a teacher)")
-    )
-    child = graphene.Field(
-        'core.graphql_query.StudentType',
-        id=graphene.Int(required=True)
-    )
-
-    def resolve_childes(self, info, page, page_size, **kwargs):
-        user = info.context.user.person
-
-        query_set = services.get_parent_childes(self, user, **kwargs)
-        return services.apply_pagination(query_set, page=page, page_size=page_size)
-
-    def resolve_child(self, info, id):
-        user = info.context.user.person
-        return services.get_parent_child(self, user, id)
--- core/graphql_query/person/person_type.py
+++ core/graphql_query/person/person_type.py
@@ -1,5 +1,6 @@
 import graphene
 import core.services as services
+from core.models import PARENT_KEY_WORD
 
 
 class PersonType(graphene.ObjectType):
@@ -21,6 +22,31 @@ class PersonType(graphene.ObjectType):
     has_phone_number = graphene.Boolean()
     # for test only:
     username = graphene.String()
+
+    childes = graphene.List(
+        'core.graphql_query.StudentType',
+        page_size=graphene.Int(default_value=services.DEFAULT_PAGE_SIZE),
+        page=graphene.Int(default_value=1),
+        kelaas_id=graphene.Int(description="privdes childern of a spacific kelaas"
+                                           "(now, it only works if query is requested by a teacher)"
+                                           "(only can be used if requested person in Parent)")
+    )
+    child = graphene.Field(
+        'core.graphql_query.StudentType',
+        id=graphene.Int(required=True),
+        description="(only can be used if requested person in Parent)",
+    )
+
+    def resolve_childes(self, info, page, page_size, **kwargs):
+        user = info.context.user.person
+        if self.type == PARENT_KEY_WORD:
+            query_set = services.get_parent_childes(self.parent, user, **kwargs)
+            return services.apply_pagination(query_set, page=page, page_size=page_size)
+
+    def resolve_child(self, info, id):
+        user = info.context.user.person
+        if self.type == PARENT_KEY_WORD:
+            return services.get_parent_child(self.parent, user, id)
 
     def resolve_username(self, info):
         if self.user:
